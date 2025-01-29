3ece7baab59df6b36954ca024d53bcb6825d0f5e
GavriloJovanovic <57016056+GavriloJovanovic@users.noreply.github.com>
2025-01-29 22:10:37 +0100

    Add builder content

--- DP_Project/src/main/creational/builder/CarBuilder.java
+++ DP_Project/src/main/creational/builder/CarBuilder.java
@@ -1,4 +1,8 @@
 package main.creational.builder;
 
 public interface CarBuilder {
+    void buildEngine();
+    void buildWheels();
+    void buildColor();
+    Car getCar();
 }
--- DP_Project/src/main/creational/builder/Director.java
+++ DP_Project/src/main/creational/builder/Director.java
@@ -1,4 +1,16 @@
 package main.creational.builder;
 
 public class Director {
+    private CarBuilder builder;
+
+    public Director(CarBuilder builder) {
+        this.builder = builder;
+    }
+
+    public Car constructCar() {
+        builder.buildEngine();
+        builder.buildWheels();
+        builder.buildColor();
+        return builder.getCar();
+    }
 }
