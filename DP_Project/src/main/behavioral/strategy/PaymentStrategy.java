a83783757db93e2ff95539b933d0ca96adb0fd4f
GavriloJovanovic <57016056+GavriloJovanovic@users.noreply.github.com>
2025-01-30 16:15:41 +0100

    Delete dummy files

--- DP_Project/src/main/behavioral/strategy/PaymentStrategy.java
+++
@@ -1,5 +0,0 @@
-package main.behavioral.strategy;
-
-public interface PaymentStrategy {
-    void pay(int amount);
-}
--- DP_Project/src/main/creational/builder/CarBuilder.java
+++
@@ -1,8 +0,0 @@
-package main.creational.builder;
-
-public interface CarBuilder {
-    void buildEngine();
-    void buildWheels();
-    void buildColor();
-    Car getCar();
-}
